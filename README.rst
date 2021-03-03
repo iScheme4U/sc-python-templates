488bc7ae41fda98c7a474b1335a7198e501b2453
Scott Lau <exceedego@126.com>
2021-03-03 10:31:46 +0800

    Initial commit
    Signed-off-by: Scott Lau <exceedego@126.com>

---
+++ README.rst
@@ -0,0 +1,53 @@
+.. image:: https://badge.fury.io/py/sc-python-templates.svg
+    :target: https://badge.fury.io/py/sc-python-templates
+.. image:: https://img.shields.io/pypi/pyversions/sc-python-templates
+    :alt: PyPI - Python Version
+
+A simple utilities for generating hash values from files
+========================================================
+
+This project provides a simple utilities for generating hash values from files.
+
+
+Installation
+------------
+
+It is possible to install the tool with `pip`::
+
+    pip install sc-python-templates
+
+Configuration
+-------------
+
+First, make sure /var/opt/sc directory exists, if not create this directory and make sure current user has the right
+to create files in this directory.
+
+You can copy `default.yml <https://github.com/Scott-Lau/sc-python-templates/blob/master/sc_templates/tests/sample_config/default.yml>`_
+to /var/opt/sc/.sc-python-templates/production.yml to initialize the production configuration.
+
+The default configuration file looks like this::
+
+    dev:
+      # whether this program is running is development mode
+      dev_mode: False
+
+
+Dependencies
+------------
+
+* `sc-utilities <https://github.com/Scott-Lau/sc-utilities>`_ >= 0.0.2
+* `sc-config <https://github.com/Scott-Lau/sc-config>`_ >= 0.0.3
+
+Changes
+-------
+
+Version 0.0.1
+    * Initial version
+
+License
+-------
+
+The script is released under the MIT License.  The MIT License is registered
+with and approved by the Open Source Initiative [1]_.
+
+.. [1] https://opensource.org/licenses/MIT
---
+++ sc_templates/main.py
@@ -0,0 +1,58 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2021 Scott Lau
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import logging
+
+from scutils import Singleton
+from scutils import log_init
+
+from sc_templates.utils import config
+
+
+class Runner(metaclass=Singleton):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        dev_mode = False
+        try:
+            dev_mode = config.get("dev.dev_mode")
+        except AttributeError:
+            pass
+        logging.getLogger(__name__).info('program is running in development mode: {}'.format(dev_mode))
+        return 0
+
+
+def main():
+    try:
+        log_init()
+        state = Runner().run()
+    except Exception as e:
+        logging.getLogger(__name__).exception('An error occurred.', exc_info=e)
+        return 1
+    else:
+        return state
+
+
+if __name__ == '__main__':
+    main()
